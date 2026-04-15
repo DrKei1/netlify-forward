ad56a0228eb4a374b47a93152fa873e70575e8da
Dʀ.Kei1 <80795572+DrKei1@users.noreply.github.com>
2026-04-15 06:01:36 +0330

    Update proxy.js

--- netlify/edge-functions/proxy.js
+++ netlify/edge-functions/proxy.js
@@ -1,17 +1,16 @@
 export default async (request, context) => {
   const targetHost = "77.68.22.95";
   const url = new URL(request.url);
-  
-  // هدایت تمام درخواست‌ها به آی‌پی آلمان روی پورت 80 با حفظ مسیر و پارامترها
   const targetUrl = `http://${targetHost}:80${url.pathname}${url.search}`;
 
   const headers = new Headers(request.headers);
   headers.set("host", targetHost);
   
-  // حذف هدرهای مزاحم نت‌لیفای
+  // حذف هدرهای اضافی نت‌لیفای برای جلوگیری از ریجکت شدن توسط سرور
   const headersToDelete = [
     "x-nf-blobs-info", "x-nf-deploy-context", "x-nf-deploy-id", 
-    "x-nf-edge-function-log-token", "x-nf-request-id", "x-nf-trace-span-id"
+    "x-nf-edge-function-log-token", "x-nf-request-id", "x-nf-trace-span-id",
+    "cdn-loop", "x-nf-purge-api-token"
   ];
   headersToDelete.forEach(h => headers.delete(h));
 
@@ -19,7 +18,7 @@ export default async (request, context) => {
     const response = await fetch(targetUrl, {
       method: request.method,
       headers: headers,
-      body: request.body, // انتقال کامل دیتای VLESS
+      body: request.body,
       redirect: "manual",
     });
 
